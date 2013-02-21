9c950768630d49b8f4ac085898734823dd949e85
saasbook <saasbook@saasbook.(none)>
2013-02-21 22:20:01 +0200

    -added new and edit pages for books -batch actions -category create action redirects to new book -categories select field

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Category do
+
   menu :priority => 2
+  
+  controller do
+
+    def create
+      create! do |format|
+         format.html { redirect_to new_admin_book_url }
+      end
+    end
+    
+  end
+  
 end
