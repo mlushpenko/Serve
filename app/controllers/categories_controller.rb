b6295229c3e7d02e9e0b8513de4914cfd6b5b0b0
saasbook <saasbook@saasbook.(none)>
2013-02-24 17:08:18 +0200

    - scope and security problems are solved

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,106 +1,23 @@
-class CategoriesController < ApplicationController
+class CategoriesController < InheritedResources::Base
+
+  respond_to :html, :xml, :json
+  
   before_filter :books, :only => :index
 
   def books
     if params[:ord]
       if params[:id]
         @category = Category.find(params[:id])
-        params[:ord] == "asc"?
-        @books = @category.books.sort_by{|b| b[:title]} :
-        @books = @category.books.sort_by{|b| b[:title]}.reverse 
+        @books = Book.category_books(@category).order_by_title(params[:ord])
       else
-        @books = Book.order("title #{params[:ord]}").all.first(16)
+        @books = Book.order_by_title(params[:ord]).limit(16)
       end
     elsif params[:id]
       @category = Category.find(params[:id])
-      @books = @category.books
+      @books = Book.category_books(@category)
     else
-      @books = Book.all.reverse.first(16)
+      @books = Book.recent
     end
   end
-  
-  # GET /categories
-  # GET /categories.json
-  def index
-    @categories = Category.all
-           
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml { render xml: @categories }
-      format.json { render json: @categories }
-    end
-  end
-
-  # GET /categories/1
-  # GET /categories/1.json
-  def show
-    @category = Category.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml { render xml: @category }
-      format.json { render json: @category }
-    end
-  end
-
-  # GET /categories/new
-  # GET /categories/new.json
-  def new
-    @category = Category.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml { render xml: @category }
-      format.json { render json: @category }
-    end
-  end
-
-  # GET /categories/1/edit
-  def edit
-    @category = Category.find(params[:id])
-  end
-
-  # POST /categories
-  # POST /categories.json
-  def create
-    @category = Category.new(params[:category])
-
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to new_book_path, notice: 'Category was successfully created.' }
-        format.json { render json: new_book_path, status: :created, location: @category }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /categories/1
-  # PUT /categories/1.json
-  def update
-    @category = Category.find(params[:id])
-
-    respond_to do |format|
-      if @category.update_attributes(params[:category])
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /categories/1
-  # DELETE /categories/1.json
-  def destroy
-    @category = Category.find(params[:id])
-    @category.destroy
-
-    respond_to do |format|
-      format.html { redirect_to categories_url }
-      format.json { head :ok }
-    end
-  end
+    
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ActiveRecord::Base
+
    attr_accessible :image, :title, :author, :description, :category_id
+   
    belongs_to :categorie
+   
    validates_presence_of :image, :title, :author, :description, :category_id
    
    has_attached_file :image,
@@ -8,4 +11,11 @@ class Book < ActiveRecord::Base
                :path => ":rails_root/public/assets/books/:id/:basename.:extension"
    validates_attachment_presence :image
    validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
+   
+   scope :recent, order('id desc').limit(16)
+   
+   scope :category_books, lambda { |category| where(:category_id => category.id) }
+   
+   scope :order_by_title, lambda {|order|(order.to_s == "asc")? order("title asc") : order("title desc") }
+   
 end
